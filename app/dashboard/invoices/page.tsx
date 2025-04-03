d29d9c6b22ea1167dddba62e9b389a2229d7fbda
ahmad-kanbari <ahmadkanbari.personal@gmail.com>
2025-04-03 22:36:17 +0400

    adding first commit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-2xl font-bold">Invoices</h1>
+      <p className="text-sm text-muted-foreground">
+        Here you can find all your invoices.
+      </p>
+    </div>
+  );
+}
